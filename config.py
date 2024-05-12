e508d975c5615ea258cb77ae07fcad65667fa319
vyacheslavi <vyach.karepov@mail.ru>
2024-05-12 10:15:05 +0700

    add dockerfiles and db_create file

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     bot_token: SecretStr
     admin_id: str
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
-config = Settings()
+settings = Settings()
--- db/database.py
+++ db/database.py
@@ -5,14 +5,6 @@ from db.basemodel import Base
 engine = create_engine("sqlite:///sqlite.db", echo=True)
 
 
-def create_db():
-    Base.metadata.create_all(engine)
-
-
-def delete_db():
-    Base.metadata.drop_all(engine)
-
-
 sessionmaker = sessionmaker(bind=engine)
 
 
