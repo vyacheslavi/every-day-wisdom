319030e28f6f93d6f5729d58690c26ac6c5825fd
vyacheslavi <vyach.karepov@mail.ru>
2024-07-31 22:11:55 +0700

    add redis_url, db_url, db_echo, anti_spam_seconds in settings

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     bot_token: SecretStr
     admin_id: str
+    redis_url: str
+
+    db_url: str
+    db_echo: bool
+
+    anti_spam_seconds: int
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
